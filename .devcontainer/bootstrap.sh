e08ae04b4f605b3f3240611c7777bc5cf5285a2e
Wessel van der Linden <wessel.vanderlinden@infosupport.com>
2024-12-26 18:19:50 +0100

    updated dockerfile with yq

--- .devcontainer/bootstrap.sh
+++ .devcontainer/bootstrap.sh
@@ -10,4 +10,27 @@ export KUBE_DNS_IP=$(cat "${SCRIPT_DIR}/kube-dns-ip.env")
 
 echo "Added kube-dns IP ($KUBE_DNS_IP) to /etc/resolv.conf"
 
+# Define the kubeconfig file path
+KUBECONFIG='/usr/local/share/kube-localhost/config'
+# Define output directory for certificates and keys
+OUTPUT_DIR="/usr/local/share/ca-certificates/kube-certs"
+sudo mkdir -p $OUTPUT_DIR
+
+# Extract client certificate
+CLIENT_CERT=$(yq '.users[0].user."client-certificate-data"' $KUBECONFIG | sed 's/"//g')
+echo $CLIENT_CERT | base64 --decode > $OUTPUT_DIR/client.crt
+
+# Extract client key
+CLIENT_KEY=$(yq '.users[0].user."client-key-data"' $KUBECONFIG | sed 's/"//g')
+echo $CLIENT_KEY | base64 --decode > $OUTPUT_DIR/client.key
+
+# Extract cluster CA certificate
+CLUSTER_CA=$(yq '.clusters[0].cluster."certificate-authority-data"' $KUBECONFIG | sed 's/"//g')
+echo $CLUSTER_CA | base64 --decode > $OUTPUT_DIR/ca.crt
+
+echo "Certificates and keys have been extracted to $OUTPUT_DIR"
+
+# Update the CA certificates
+sudo update-ca-certificates
+
 sleep infinity
